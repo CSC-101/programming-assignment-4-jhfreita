f7625b856c8d120dab9b3e06bbe56f7f3e1b84ea
jfreitas3138 <jfreitas@mediacombb.net>
2024-11-22 09:46:37 -0800

    Update 6

--- hw4.py
+++ hw4.py
@@ -6,6 +6,7 @@ from build_data import convert_county
 full_data = build_data.get_data()
 sample_file = input("Enter a file name: ")
 open(sample_file, "r")
+import traceback
 # The following function is designed to read each line in a file and do various things depending on
 # its contents. If the line requests the total population of some data, the population of the counties in
 # the data will be printed. If the line requests filter by state, the data will be filtered accordingly, and the counties
@@ -13,42 +14,50 @@ open(sample_file, "r")
 # If the line requests filtering by a certain attribute being greater or less than a number, a list of counties
 # meeting that requirement will be printed. If the line requests a certain percent or total population of all the counties
 # in a set of data, that will be returned. Lastly, if an error occurs due to a line, the user should be notified.
-# Error control is admittedly compromised, as I have been behind on my assignments lately. Sorry.
+# Finally, there is error control, which I had to do some outside research to find syntax that would report where an
+# error occurred and continue onto the next line.
 with open(sample_file, "r") as file:
-        first_line = file.readline().strip()
-        data = full_data
-        def display(county):
-            print(f"County Name: {county.county}, State: {county.state}")
-            print("\nPopulation:")
-            for key, value in county.population.items():
-                print(f"{key}: {value}")
-            print("\nAge:")
-            for key, value in county.age.items():
-                print(f"{key}: {value}%")
-            print("\nEducation:")
-            for key, value in county.education.items():
-                print(f"{key}: {value}%")
-            print("\nEthnicities:")
-            for key, value in county.ethnicities.items():
-                print(f"{key}: {value}%")
-            print("\nIncome:")
-            for key, value in county.income.items():
-                print(f"{key}: {value}")
-        def population_total(CountyDemographics_list: list):
-            population_list = [i.population for i in CountyDemographics_list]
-            total_population = 0
-            for i in range(len(CountyDemographics_list)):
-                total_population = total_population + CountyDemographics_list[i].population['2014 Population']
-            return total_population
-        def filter_by_attribute1(counties: list, attribute: str, specification, threshold: str):
-            modified_list = [i for i in counties if getattr(i, attribute)[specification] > float(threshold)]
-            return modified_list
-        def filter_by_attribute2(counties: list, attribute: str, specification, threshold: str):
-            modified_list = [i for i in counties if getattr(i, attribute)[specification] < float(threshold)]
-            return modified_list
-        if first_line[:16] == "population-total":
-            print("2014 Population:", population_total(data))
-        elif first_line[:9] == "filter-gt":
+    first_line = file.readline().strip()
+    data = full_data
+    def display(county):
+        print(f"County Name: {county.county}, State: {county.state}")
+        print("\nPopulation:")
+        for key, value in county.population.items():
+            print(f"{key}: {value}")
+        print("\nAge:")
+        for key, value in county.age.items():
+            print(f"{key}: {value}%")
+        print("\nEducation:")
+        for key, value in county.education.items():
+            print(f"{key}: {value}%")
+        print("\nEthnicities:")
+        for key, value in county.ethnicities.items():
+            print(f"{key}: {value}%")
+        print("\nIncome:")
+        for key, value in county.income.items():
+            print(f"{key}: {value}")
+    def population_total(CountyDemographics_list: list):
+        population_list = [i.population for i in CountyDemographics_list]
+        total_population = 0
+        for i in range(len(CountyDemographics_list)):
+            total_population = total_population + CountyDemographics_list[i].population['2014 Population']
+        return total_population
+    def filter_by_attribute1(counties: list, attribute: str, specification, threshold: str):
+        modified_list = [i for i in counties if getattr(i, attribute)[specification] > float(threshold)]
+        return modified_list
+    def filter_by_attribute2(counties: list, attribute: str, specification, threshold: str):
+        modified_list = [i for i in counties if getattr(i, attribute)[specification] < float(threshold)]
+        return modified_list
+    def population_by_attribute(counties: list, attribute: str, specification: str):
+        population_list = [
+            0.01 * i.population['2014 Population'] * getattr(i, attribute)[specification]
+            for i in counties]
+        total_population = sum(population_list)
+        return total_population
+    if first_line[:16] == "population-total":
+        print("2014 Population:", population_total(data))
+    elif first_line[:9] == "filter-gt":
+        try:
             parts = first_line.split(":", 2)
             part1 = parts[0]
             part4 = parts[2]
@@ -57,7 +66,11 @@ with open(sample_file, "r") as file:
             raw_list = filter_by_attribute1(data, part5, part3, part4)
             presentation_list = [display(i) for i in raw_list]
             print(presentation_list)
-        elif first_line[:9] == "filter-lt":
+        except Exception as e:
+            error_description = traceback.format_exc()
+            print("An error occurred in line", traceback.extract_tb(e.__traceback__)[-1].lineno)
+    elif first_line[:9] == "filter-lt":
+        try:
             parts = first_line.split(":", 2)
             part1 = parts[0]
             part4 = parts[2]
@@ -66,8 +79,11 @@ with open(sample_file, "r") as file:
             raw_list = filter_by_attribute2(data, part5, part3, part4)
             presentation_list = [display(i) for i in raw_list]
             print(presentation_list)
-        else:
-            if first_line[:12] == "filter-state":
+        except Exception as e:
+            error_description = traceback.format_exc()
+            print("An error occurred in line", traceback.extract_tb(e.__traceback__)[-1].lineno)
+    elif first_line[:12] == "filter-state":
+        try:
                 parts = first_line.split("-", 1)
                 part1 = parts[0]
                 part2, part3 = parts[1].split(":", 1)
@@ -77,37 +93,38 @@ with open(sample_file, "r") as file:
                 else:
                     presentation_list = [display(i) for i in data]
                     print(presentation_list)
-            for line in file:
-                try:
-                    line = line.rstrip("\n")
-                    if line[:16] == "population-total":
-                        print("2014 Population:", population_total(data))
-                    elif line[:9] == "filter-gt":
-                        parts = line.split(":", 2)
-                        part1 = parts[0]
-                        part4 = parts[2]
-                        part2, part3 = parts[1].split(".", 1)
-                        print(filter_by_attribute1(data, part2, part3, part4))
-                    elif line[:9] == "filter-lt":
-                        parts = line.split(":", 2)
-                        part1 = parts[0]
-                        part4 = parts[2]
-                        part2, part3 = parts[1].split(".", 1)
-                        print(filter_by_attribute2(data, part2, part3, part4))
-                    else:
-                        parts = line.split(":", 1)
-                        part1 = parts[0]
-                        part2, part3 = parts[1].split(".", 1)
-                        part4 = part2.lower()
-                        def population_by_attribute(counties: list, attribute: str, specification: str):
-                            population_list = [
-                                0.01 * i.population['2014 Population'] * getattr(i, attribute)[specification]
-                                for i in counties]
-                            total_population = sum(population_list)
-                            return total_population
-                        if part1 == "population":
-                            print("2014", part2, ".", part3, ":", population_by_attribute(data, part4, part3))
-                        if part1 == "percent":
-                            print("2014", part2, ".", part3, ":", 100 * population_by_attribute(data, part4, part3)/population_total(data))
-                except ValueError:
-                    print("An error occurred")
+        except Exception as e:
+            error_description = traceback.format_exc()
+            print("An error occurred in line", traceback.extract_tb(e.__traceback__)[-1].lineno)
+    for line in file:
+            try:
+                line = line.rstrip("\n")
+                if line[:16] == "population-total":
+                    print("2014 Population:", population_total(data))
+                elif line[:9] == "filter-gt":
+                    parts = line.split(":", 2)
+                    part1 = parts[0]
+                    part4 = parts[2]
+                    part2, part3 = parts[1].split(".", 1)
+                    print(filter_by_attribute1(data, part2, part3, part4))
+                elif line[:9] == "filter-lt":
+                    parts = line.split(":", 2)
+                    part1 = parts[0]
+                    part4 = parts[2]
+                    part2, part3 = parts[1].split(".", 1)
+                    print(filter_by_attribute2(data, part2, part3, part4))
+                else:
+                    parts = line.split(":", 1)
+                    part1 = parts[0]
+                    part2, part3 = parts[1].split(".", 1)
+                    part4 = part2.lower()
+                    if part1 == "population":
+                        print("2014", part2, ".", part3, ":", population_by_attribute(data, part4, part3))
+                    if part1 == "percent":
+                        print("2014", part2, ".", part3, ":",
+                              100 * population_by_attribute(data, part4, part3) / population_total(data))
+            except Exception as e:
+                error_description = traceback.format_exc()
+                print("An error occurred in line", traceback.extract_tb(e.__traceback__)[-1].lineno)
+                continue
+
